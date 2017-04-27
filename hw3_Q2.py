f70e0ab00a640c5f7417d1e7f6d120924add6da8
Jordan Chen <j3492274@gmail.com>
2017-04-27 14:50:37 +0800

    new update

--- hw3_Q2.py
+++ hw3_Q2.py
@@ -6,7 +6,7 @@ sc = SparkContext(conf = conf)
 sqc = SQLContext(sc)
 
 a = sqc.read.format("com.databricks.spark.csv") \
-	.option(header = 'true', inferschema = 'true') \
+	.options(header = 'true', inferschema = 'true') \
    	.load("yellow_tripdata_2016-08.csv")
 
 b = a.select('passenger_count', 'payment_type').filter(a.passenger_count > 0)
